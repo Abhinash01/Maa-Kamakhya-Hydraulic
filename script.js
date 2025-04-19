f075f9f1159a6cdf972f475cc493005b2a5f7e21
Abhinash01 <abinashgupa111@gmail.com>
2025-04-19 23:27:50 +0530

    Landing Page Scripting Done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-gsap
+function head(){
+    var tl = gsap.timeline();
+
+    tl.from('.headline h1',{
+        y: '30',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+    tl.from('.quote',{
+        y: '-10',
+        opacity: 0,
+        duration: 1.8,
+        ease: Expo.easeInOut
+    })
+}
+
+head();
